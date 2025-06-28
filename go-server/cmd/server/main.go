9c8bc063194d845b71ca07d05dfcd1fb3059e4a3
j0y <12984600+j0y@users.noreply.github.com>
2025-06-28 10:06:09 +0200

    Add log level option

--- go-server/cmd/server/main.go
+++ go-server/cmd/server/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/rs/zerolog"
@@ -21,11 +23,25 @@ const (
 )
 
 func main() {
+	// Parse command line flags
+	var logLevel string
+	flag.StringVar(&logLevel, "log-level", "info", "Set log level (trace, debug, info, warn, error, fatal, panic)")
+	flag.Parse()
+
 	// Configure global logger
 	zerolog.TimeFieldFormat = "15:04:05"
 	zlog.Logger = zlog.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: "15:04:05"})
 
+	// Set log level based on flag
+	level, err := parseLogLevel(logLevel)
+	if err != nil {
+		log.Fatalf("Invalid log level '%s': %v", logLevel, err)
+	}
+	zerolog.SetGlobalLevel(level)
+
 	printBanner()
+
+	zlog.Info().Str("logLevel", logLevel).Msg("Server starting with log level")
 
 	// Create server instance
 	srv := server.New()
@@ -64,4 +80,30 @@ func printBanner() {
 	fmt.Printf("[!] : CoopAndreas Server Go Prototype\n")
 	fmt.Printf("[!] : Version : %s\n", Version)
 	fmt.Printf("[!] : Platform : Go Runtime\n")
+	fmt.Println()
+	fmt.Println("[!] : Usage:")
+	fmt.Println("  -log-level string")
+	fmt.Println("        Set log level (trace, debug, info, warn, error, fatal, panic) (default \"info\")")
+}
+
+// parseLogLevel converts a string log level to zerolog.Level
+func parseLogLevel(level string) (zerolog.Level, error) {
+	switch strings.ToLower(level) {
+	case "trace":
+		return zerolog.TraceLevel, nil
+	case "debug":
+		return zerolog.DebugLevel, nil
+	case "info":
+		return zerolog.InfoLevel, nil
+	case "warn", "warning":
+		return zerolog.WarnLevel, nil
+	case "error":
+		return zerolog.ErrorLevel, nil
+	case "fatal":
+		return zerolog.FatalLevel, nil
+	case "panic":
+		return zerolog.PanicLevel, nil
+	default:
+		return zerolog.InfoLevel, fmt.Errorf("unknown log level: %s (valid levels: trace, debug, info, warn, error, fatal, panic)", level)
+	}
 }
